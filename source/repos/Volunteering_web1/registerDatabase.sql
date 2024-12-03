3435885e3660bbd6d034adacf6da524afd8657c0
Bashar Ahmed <150681879+Bashardev@users.noreply.github.com>
2024-12-03 18:15:36 +0400

    Register Sql code

---
+++ source/repos/Volunteering_web1/registerDatabase.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[register] (
+    [id]         INT          IDENTITY (1, 1) NOT NULL,
+    [firstName]  VARCHAR (12) NOT NULL,
+    [familyName] VARCHAR (12) NOT NULL,
+    [email]      VARCHAR (20) NOT NULL,
+    [password]   VARCHAR (12) NOT NULL
+);
+
